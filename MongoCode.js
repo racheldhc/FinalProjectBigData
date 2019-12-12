d1379ba71a2ed05d6bfc8a47cfffdb32777cabad
Rachel Callaghan <rachelcallaghan81@gmail.com>
2019-12-12 15:43:37 +0000

    Plan answers to each question
    Write some information about each question to plan what to implement for each question

--- MongoCode.js
+++ MongoCode.js
@@ -1,3 +1,4 @@
+// 1) Store in appropriate database ************************************************************************************
 // CHANGING DATE FORMAT
 // In excel, split the given date format into date, month, year and time (in different columns)
 // Merge them back together in the format year-month-day"T"hour"Z"
@@ -27,3 +28,32 @@
         db.hourly_dublin_17_18.save(element);
         })
     db.hourly_dublin_17_18.find({"date" : ISODate("1970-01-01T00:00:00Z")}).count()
+
+// 2) How database allows for differently structured data **************************************************************
+// Mongo is a schemaless database, this means that there is no structure on the data that can be inserted.
+// Because of this, mongo will allow for differently structured data to be added to the database.
+
+// 3) How your solution will scale ************************************************************************************* 
+// Mongo supports sharding. Sharding is splitting the data up between different servers, controlled by one config server.
+// This allows my solution to scale out as data can be put on more servers connected to the config server.
+// Sharding is accomplished by 
+// 1) setting up config server and it's replica sets
+// 2) setting up Shard 1 and it's replica sets
+// 3) setting up Shard 2 and it's replica sets
+// 4) Connecting each shard to the config server
+// 5) Importing the data
+
+// 4) Back up the data and high availability to servers ****************************************************************
+// Replica sets, that are made above, provide both backups and high availability. The replica sets provide high 
+// availability to each shard
+
+// 5) prepare, clean, aggregate and analyse the data *******************************************************************
+
+
+
+
+
+
+
+
+
